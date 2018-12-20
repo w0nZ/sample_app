a801f8b1c38a73a64c4cb4b36751cb78e76f3d6a
w0nZ <fabian.bialowons@gmail.com>
2018-12-20 17:27:58 +0100

    Updating and showing Users. Next 10.4 Deleting

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
   #Comments
   #@user = User.find_by(email: params[:session][:email].downcase)
   #  if assigns(:user) && assigns(:user).authenticate(params[:session][:password])
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,6 @@
 module UsersHelper
 
+  # Returns the Gravatar for the given user.
   def gravatar_for(user, options = { size: 80 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
