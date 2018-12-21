5d0dd02d2109a0035f1fd4c9d1c35e6775271ab9
w0nZ <fabian.bialowons@gmail.com>
2018-12-21 10:46:27 +0100

    add account activation

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -21,7 +21,9 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-
+  config.action_mailer.delivery_method = :test
+  host = 'localhost:3000'                     # Local server
+  config.action_mailer.default_url_options = { host: host }
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
